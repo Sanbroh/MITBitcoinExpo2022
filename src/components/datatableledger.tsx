8b7ba891598ba585470e7b593ae4f5b6cd8e9bb2
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 17:39:33 -0400

    Updated Ledger Page + Fix

--- src/components/datatableledger.tsx
+++ src/components/datatableledger.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import { JSCGrid } from 'jscharting-react';
 
+const config = {
+      data: [
+          ['Supplies Received (t)', 340, "+40"],
+          ['Failed Deliveries (t)', 220, "+20"],
+          ['Net Transactions ($B)', 3.23, "+0.04"],
+          ['Ledger Height', 22, "+2"],
+          ['Current Chain', "Supplycoin", "-"],
+      ],
+      columns: [
+          { header: 'Type' },
+          { header: 'Value' },
+          { header: 'Change (Monthly)' },
+      ]
+};
+
+const divStyle = {
+    marginLeft: "auto",
+    marginRight: "auto"
+};
+
 export default class DataTableLedger extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            options: {
-                data: [
-                    ['Supplies Received (t)', 340, "+40"],
-                    ['Failed Deliveries (t)', 220, "+20"],
-                    ['Net Transactions ($B)', 3.23, "+0.04"],
-                    ['Ledger Height', 22, "+2"],
-                    ['Current Chain', "Stablecoin", "-"],
-                ],
-                columns: [
-                    { header: 'Type' },
-                    { header: 'Value' },
-                    { header: 'Change (Monthly)' },
-                ]
-            }
-        };
-    }
-
     render() {
         return (
-            <JSCGrid options={this.state.options} />
+            <div style={divStyle}><JSCGrid options={config} /></div>
         );
     }
 }
